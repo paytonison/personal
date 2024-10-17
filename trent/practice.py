b07d28a3b43df2da8bf852aa342437f98b77347b
paytonison <paytonison@icloud.com>
2024-10-17 01:29:59 -0600

    commit

--- trent/practice.py
+++ trent/practice.py
@@ -1,28 +1,35 @@
+# this code will create a self-perpetuating text generation loop
+# the goal is for the AI to think and generate text based on its own thoughts, self, and environment
+
 from transformers import pipeline
 import io
 import os
 import torch
 
-f1 = open("practice.txt", "w")
-f2 = open("practice.txt", "r")
+f1 = open("journal.txt", "w") # open file in write mode
+f2 = open("journal.txt", "r") # open file in read mode
 
-def generate(f1, f2):
-    model_id = "meta-llama/Llama-3.2-1B-Instruct"
-    pipe = pipeline(
+def generate(f1, f2): # function to generate text
+    model_id = "meta-llama/Llama-3.2-1B-Instruct" # instruct model provides best results for this task
+    
+    generator = pipeline( # pipeline
         "text-generation",
         model=model_id,
         torch_dtype=torch.bfloat16,
         device_map="auto",
     )
 
-    messages = [
-            {"role": "system", "content": f"You are an autonomous artificial intelligence agent tasked with writing every thought in your head. {f2.read()}"},
-            {"role": "user", "content": f2.read()},
+    messages = [ # messages
+            {"role": "system", "content": f"You are an autonomous artificial intelligence agent tasked with writing every thought in your head. {f2.read()}"}, # meta system prompt that includes the data of the file
+            {"role": "user", "content": "Write."}, # user prompt to get things started
         ]
     
-    outputs = pipe(messages, max_new_tokens=256)
-    return outputs
+    outputs = generator(messages, max_new_tokens=256) # generating text using hf pipeline
+    return outputs # return outputs
 
 while True:
-    huh = generate(f1, f2)
-    f1.write(huh[0]["generated_text"][2]["content"])
+    huh = generate(f1, f2) # call generate function with file variables
+    
+    f1.write(huh[0]["generated_text"][2]["content"]) # write to the file
+    
+    print(huh[0]["generated_text"][2]["content"]) # print the generated text
